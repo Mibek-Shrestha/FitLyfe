4868fe1b27b358b6ebabb85c8efe9558813275da
mibek-shrestha <mibekstha@gmail.com>
2023-06-16 22:14:39 +0545

    SearchExercise ui completed ,didn't uses hooks yet

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './App.css';
-import {BrowserRouter as Router,Route,Routes} from 'react-router-dom';
+import {Route,Routes} from 'react-router-dom';
 import {Box} from '@mui/material';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
@@ -10,12 +10,11 @@ import Footer from './components/Footer';
   return (
     <Box width='400px' sx={{ width:{x1:'1488px'}}}m='auto'>
       <Navbar />
-      <Router>
             <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/exercise/:id" element={<ExerciseDetail/>}/>
        </Routes>
-      </Router>
+     
     <Footer/>
     </Box>
   )
